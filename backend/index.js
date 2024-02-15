003c107865185b5d6cfa6a6ed43cc015c06133b2
Yashwadkar2121 <yashwadkar132@gmail.com>
2024-02-16 00:23:51 +0530

    Creating Models and routes

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,9 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// Avaiable Routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/note"));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
