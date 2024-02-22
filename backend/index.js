abff6521e8ce3eed8faf988ccb9066aae302de67
Yashwadkar2121 <yashwadkar132@gmail.com>
2024-02-22 12:47:03 +0530

    Some normal change and check email exist or not

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 
 connectToMongo();
 const app = express();
-const port = 3000;
+const port = 5000;
 
 // middleware for send json body to DB
 app.use(express.json());
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -3,29 +3,39 @@ const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 
-// Create a User using: POST "/api/auth/" . Doesn't require Auth
+// Create a User using: POST "/api/auth/createuser". No Login Requried
 router.post(
-  "/",
+  "/createuser",
   [
     body("email", "Enter a valid Email").isEmail(),
     body("password", "password atleast 6 characters").isLength({ min: 6 }),
     body("name", "Enter a valid Name").isLength({ min: 3 }),
   ],
-  (req, res) => {
+  async (req, res) => {
+    // If there are errors, return bad request and the errors
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    User.create({
-      name: req.body.name,
-      password: req.body.password,
-      email: req.body.email,
-    })
-      .then((user) => res.json(user))
-      .catch((err) => {
-        console.log(err);
-        res.json({ error: "Please enter the unique value" });
+    // Check whether the email is already exist
+    try {
+      let user = await User.findOne({ email: req.body.email });
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "Sorry a user with this email is already exists " });
+      }
+      // Create a new user
+      user = await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
       });
+      res.json({ user });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Some error occured");
+    }
   }
 );
 
