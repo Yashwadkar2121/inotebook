78b3acf1231ab1267af61cd44f3b846bbdf0d620
Yashwadkar2121 <yashwadkar132@gmail.com>
2024-02-22 10:35:35 +0530

    Store data into database

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,9 @@ connectToMongo();
 const app = express();
 const port = 3000;
 
+// middleware for send json body to DB
+app.use(express.json());
+
 // Avaiable Routes
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/note"));
