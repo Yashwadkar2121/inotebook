fc5dbdc3bf08b34c7ea362e631be05d4b2514555
Yashwadkar2121 <yashwadkar132@gmail.com>
2024-02-24 01:54:30 +0530

    Adding 2 Rotes :- Fetch all Notes And Add Notes

--- backend/index.js
+++ backend/index.js
@@ -10,8 +10,8 @@ const port = 5000;
 app.use(express.json());
 
 // Avaiable Routes
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/notes", require("./routes/note"));
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
