45eaae17404c1a2353decbbe1dcb9829292bfb83
Yashwadkar2121 <yashwadkar143@gmail.com>
2024-02-29 03:10:31 +0530

    Create React app

--- db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const mongoURI = "mongodb://localhost:27017";
-
-const mongooseToConnect = () => {
-  mongoose.connect(mongoURI);
-};
-
-module.exports = mongooseToConnect;
