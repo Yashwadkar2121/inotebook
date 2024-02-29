f3fa130e55bc286a7cd78030ff55ac006299968f
Yashwadkar2121 <yashwadkar143@gmail.com>
2024-03-01 01:29:43 +0530

    Useloction Hook

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,17 +1,5 @@
-import React, { useContext, useEffect } from "react";
-import noteContext from "../context/notes/noteContext";
-
 const About = () => {
-  const a = useContext(noteContext);
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line
-  }, []);
-  return (
-    <div>
-      This is about {a.name.name} and he is in class {a.name.class}
-    </div>
-  );
+  return <div>This is about</div>;
 };
 
 export default About;
--- frontend/src/context/notes/NoteState.js
+++ frontend/src/context/notes/NoteState.js
@@ -1,21 +1,8 @@
-import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
-  const s1 = {
-    name: "yash",
-    class: "10th",
-  };
-  const [name, setName] = useState(s1);
-  const update = () => {
-    setTimeout(() => {
-      setName({ name: "Akash", class: "12th" });
-    }, 3000);
-  };
   return (
-    <NoteContext.Provider value={{ name, update }}>
-      {props.children}
-    </NoteContext.Provider>
+    <NoteContext.Provider value={{}}>{props.children}</NoteContext.Provider>
   );
 };
 
