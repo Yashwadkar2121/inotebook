cd25e4a0f79ec58f9666b3bb278b9bc3b4b5bceb
Yashwadkar2121 <yashwadkar143@gmail.com>
2024-03-02 03:21:00 +0530

    Adding a Modal for Editing Notes

--- frontend/src/components/Noteitem.js
+++ frontend/src/components/Noteitem.js
@@ -4,21 +4,27 @@ import noteContext from "../context/notes/noteContext";
 const Noteitem = (props) => {
   const context = useContext(noteContext);
   const { deleteNote } = context;
-  const { note } = props;
+  const { note, updateNote } = props;
   return (
     <div className="col-md-3 my-3">
       <div className="card">
         <div className="card-body">
-          <h5 className="card-title">
+          <h4 className="card-title">
             {note.title}
-            <i className="fa-solid fa-file-pen mx-2"></i>
+            <i
+              className="fa-solid fa-file-pen mx-2"
+              onClick={() => {
+                updateNote(note._id);
+              }}
+            ></i>
             <i
               className="fa-solid fa-trash-can mx-2"
               onClick={() => {
                 deleteNote(note._id);
               }}
             ></i>
-          </h5>
+          </h4>
+          <h5>{note.tag}</h5>
           <h6 className="card-subtitle mb-2 text-body-secondary">
             {note.date}
           </h6>
--- frontend/src/context/notes/NoteState.js
+++ frontend/src/context/notes/NoteState.js
@@ -47,15 +47,28 @@ const NoteState = (props) => {
     };
     setNotes(notes.concat(note));
   };
+
   // Delete a Note
-  const deleteNote = (id) => {
-    // TODO: API Call
+  const deleteNote = async (id) => {
+    // API Call
+    const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        "auth-token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjVkZmNhMTI5MDVhODEyNDRmNDQ1M2U2In0sImlhdCI6MTcwOTE2NTA4M30.ifkEsAE-WlN66H9vuq0PmWbN5nWSqkTgLCX7KzoOgNs",
+      },
+    });
+    // Logic to delete note in Client
+    const json = response.json();
+    console.log(json);
     console.log("Deleting the note with id" + id);
     const newNotes = notes.filter((note) => {
       return note._id !== id;
     });
     setNotes(newNotes);
   };
+
   // Edit a Note
   const editNote = async (id, title, description, tag) => {
     // API Call
