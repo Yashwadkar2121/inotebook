453b8a651a66f4b9ca21e906bc574d62a5a54b80
Yashwadkar2121 <yashwadkar132@gmail.com>
2024-03-28 04:15:54 +0530

    Deployment basic

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ var cors = require("cors");
 
 connectToMongo();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 // middleware for send json body to DB
 app.use(express.json());
