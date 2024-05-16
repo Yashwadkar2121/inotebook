9ae5169c16ad4002042b11b2d96f4a3c617362b7
Yashwadkar2121 <yashwadkar132@gmail.com>
2024-05-17 03:20:28 +0530

    front url paste in backend

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const port = 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: [""],
+    origin: ["https://inotebook-frontend-mauve.vercel.app/login"],
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
   })
