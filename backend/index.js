7e27abcbf6bbb86a4e9966a865855b1bc9301e77
Yashwadkar2121 <yashwadkar132@gmail.com>
2024-05-17 03:24:05 +0530

    remove in uri /login

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ const port = 5000;
 app.use(express.json());
 app.use(
   cors({
-    origin: ["https://inotebook-frontend-mauve.vercel.app/login"],
+    origin: ["https://inotebook-frontend-mauve.vercel.app"],
     methods: ["POST", "GET", "DELETE", "PUT"],
     credentials: true,
   })
